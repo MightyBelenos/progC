1f4f60d25a4741d078e2eea1a01c69d977b8fa8d
MightyBelenos <steven.breton@insa-cvl.fr>
2022-09-22 10:17:04 +0200

    v5 initial comit

---
+++ v5/symbol/symbol.c
@@ -0,0 +1,18 @@
+#include "symbol.h"
+#include "generator.h" //generate
+#include <stdlib.h> //malloc
+#include <stdio.h> //sprintf
+
+#define PREFIX "FR__"
+#define SUFFIX "_"
+
+char *symbol_new_symbol(void)
+{
+	int n = generator_generate();
+
+	static char *buffer = NULL;
+	if (buffer == NULL)
+		buffer = malloc(81 * sizeof(char));
+	sprintf(buffer, "%s%d%s", PREFIX, n, SUFFIX);
+	return buffer;
+}
