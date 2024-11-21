fc79b7e828683e29d1ea73af12f758178ca4f551
MightyBelenos <114140072+MightyBelenos@users.noreply.github.com>
2024-11-22 00:14:32 +0100

    Delete v5 directory

--- v5/generator/generator.h
+++
@@ -1,9 +0,0 @@
-#ifndef GENERATOR_H
-#define GENERATOR_H
-
-extern void generator_define_first(int new_first_value);
-extern void generator_define_step(int new_step);
-extern void generator_go_to_beginning(void);
-extern int generator_get_next(void);
-
-#endif
